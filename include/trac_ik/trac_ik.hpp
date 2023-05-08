cc18d159b833a366788fdac58e87225792d7c1ee
Pardi Tommaso <tommaso.pardi@gmail.com>
2023-05-08 18:51:23 +0100

    chore(licence): added the derived licence

--- include/trac_ik/trac_ik.hpp
+++ include/trac_ik/trac_ik.hpp
@@ -1,5 +1,6 @@
 /********************************************************************************
 Copyright (c) 2015, TRACLabs, Inc.
+Copyright (c) 2023, Tommaso Pardi
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification,
@@ -49,7 +50,7 @@ class TRAC_IK
 public:
   TRAC_IK(const KDL::Chain& _chain, const KDL::JntArray& _q_min, const KDL::JntArray& _q_max, double _maxtime = 0.005, double _eps = 1e-5, SolveType _type = Speed);
 
-  TRAC_IK(const std::string& base_link, const std::string& tip_link, const std::string& URDF_param = "/robot_description", double _maxtime = 0.005, double _eps = 1e-5, SolveType _type = Speed);
+  TRAC_IK(const std::string& base_link, const std::string& tip_link, const std::string& URDF_path, double _maxtime = 0.005, double _eps = 1e-5, SolveType _type = Speed);
 
   ~TRAC_IK();
 
